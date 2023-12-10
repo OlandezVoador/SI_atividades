b66111375cda12062efd7950bbe813f8a9996099
OlandezVoador <felipe.banes@outlook.com>
2023-12-10 13:45:03 -0300

    Atividade 1

---
+++ dicas.py
@@ -0,0 +1,5 @@
+lista.append(variavel) # Adicao no final da fila
+lista1.extend(lista2) # Uniao de filas
+lista1.index(3) # Descreve o que está na 3° posicao
+lsta1.sort()# Ordenacao
+matriz.shape # Tamanho da matriz
